d910a6440d38e782096f4545db78e4bb9698c015
Brian Washington <bw49078@gmail.com>
2022-10-05 17:48:40 -0500

    finished problem

--- problems/08-count.js
+++ problems/08-count.js
@@ -28,11 +28,38 @@ console.log(result4); // 0
 
 function count(array, cb) {
   // Your code here
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (cb(item)) sum++;
+  }
+
+  return sum;
 }
 
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('x');
+});
+console.log(result4); // 0
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = count;
-} catch(e) {
+} catch (e) {
   return null;
 }
